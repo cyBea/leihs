55d1f2e1fde69b2bdbe7b8c076ba69017d8b4ff2
Ramón Cahenzli <rca@psy-q.ch>
2014-09-11 15:11:11 +0200

    Run both headless kinds of tests in sequence on Travis.

--- script/run_nojs_tests.sh
+++ script/run_nojs_tests.sh
@@ -8,7 +8,7 @@ bundle exec cucumber -t ~@javascript features/
 if [[ $? -ne 0 ]]; then
   if [[ -f tmp/rerun.txt && -s tmp/rerun.txt ]]; then
     echo "Rerun necessary, the first execution exited with non-0."
-    bundle exec cucumber -t ~@javascript -p rerun
+    bundle exec cucumber -p rerun
     exit $?
   else
     echo "Something went wrong during test setup or execution before even running Cucumber."
