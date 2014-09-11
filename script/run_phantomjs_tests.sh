202477cfc1edb7e2543b5fd8f3a64217d3b946bf
Ramón Cahenzli <rca@psy-q.ch>
2014-09-11 15:11:11 +0200

    Run both headless kinds of tests in sequence on Travis.

--- script/run_phantomjs_tests.sh
+++ script/run_phantomjs_tests.sh
@@ -4,9 +4,13 @@ bash script/validate_gettext_files.sh && \
 bundle exec rake leihs:reset && \
 bundle exec cucumber -t @javascript -t ~@browser features/
 
-if [[ -f tmp/rerun.txt && -s tmp/rerun.txt && $? -ne 0 ]]; then
-  echo "Rerun necessary, the first execution exited with non-0."
-  bundle exec cucumber -t @javascript -t ~@browser -p rerun
+if [[ $? -ne 0 ]]; then
+  if [[ -f tmp/rerun.txt && -s tmp/rerun.txt ]]; then
+    echo "Rerun necessary, the first execution exited with non-0."
+    bundle exec cucumber -p rerun
+    exit $?
+  else
+    echo "Something went wrong during test setup or execution before even running Cucumber."
+    exit 1
+  fi
 fi
-
-exit $?
