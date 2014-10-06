24f0b7b264282e9912c7eb1b558b918e575bfce3
Ramón Cahenzli <rca@psy-q.ch>
2014-10-06 15:43:55 +0200

    Cider CI attachment retrieval stops on 404.

--- script/retrieve_cider_coverage.rb
+++ script/retrieve_cider_coverage.rb
@@ -26,41 +26,54 @@ class CiderClient
     return "http://#{@username}:#{@password}@ci2.zhdk.ch"
   end
 
-  def url(path)
+  # URL starting from the execution, with the passed path appended
+  def entry_url(path)
     return "#{base_url}/cider-ci/api/v1/execution/#{@execution_id}/#{path}"
   end
 
+  # URL starting from the base URL root, with the passed path appended
+  def url(path)
+    return "#{base_url}/#{path}"
+  end
+
   def tasks
-    t = JSON.parse(RestClient.get(url("tasks")))
+    t = JSON.parse(RestClient.get(entry_url("tasks")))
     t["_links"]["cici:task"]
   end
 
-  def collect_trials
+  def trials
     trials = []
     tasks.each do |task|
-      task_url = "#{base_url}#{task['href']}"
+      task_url = url(task['href'])
       details = JSON.parse(RestClient.get(task_url))
-      trials_url = "#{base_url}#{details["_links"]["cici:trials"]["href"]}"
-      trial_details = JSON.parse(RestClient.get(trials_url))
-      trial_details["_links"]["cici:trial"].each do |td|
-        trials << td
+      trials_url = url(details["_links"]["cici:trials"]["href"])
+      single_trial = JSON.parse(RestClient.get(trials_url))
+      single_trial["_links"]["cici:trial"].each do |st|
+        trials << st
       end
     end
     trials
   end
 
-  def collect_attachments(pattern = nil)
-    collect_trials.each do |trial|
-
+  def trial_attachments(pattern = nil)
+    trial_attachments = []
+    attachments = []
+    trials.each do |trial|
+      trial_url = url(trial["href"])
+      single_trial = JSON.parse(RestClient.get(trial_url))
+      trial_attachments << single_trial["_links"]["cici:trial-attachments"]
+    end
+    trial_attachments.each do |ta|
+      attachment_detail_url = url(ta["href"])
+      attachment_details = JSON.parse(RestClient.get(attachment_detail_url))  # 404 happens here
     end
   end
 
 end
 
-
 cc = CiderClient.new
 cc.username = username
 cc.password = password
-cc.execution_id = "37b8408a-8e22-465b-9ae9-ba90b58262fd"
-cc.tasks
-cc.collect_trials
+cc.execution_id = "d7a1c121-8f22-471a-8d25-292cb5669883"
+cc.trial_attachments
+
