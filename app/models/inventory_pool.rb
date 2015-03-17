fb6ad1f4601f5675c1c3d1547e0cab56c41f868d
Franco Sellitto <franco.sellitto@zhdk.ch>
2015-03-17 16:08:33 +0100

    import CSV properties_anschaffungskategorie column

--- app/models/inventory_pool.rb
+++ app/models/inventory_pool.rb
@@ -279,27 +279,24 @@ class InventoryPool < ActiveRecord::Base
     transaction do
       CSV.foreach(csv_file, col_sep: ",", quote_char: "\"", headers: :first_row) do |row|
         unless row["inventory_code"].blank?
-          location = if row["building"].blank? and row["room"].blank?
-                       nil
-                     else
-                       building_id = row["building"].blank? ? nil : Building.find_or_create_by(name: row["building"]).id
-                       room = row["room"].blank? ? nil : row["room"]
-                       Location.find_or_create(building_id: building_id, room: room)
-                     end
-
-          supplier = row["supplier_name"].blank? ? nil : Supplier.find_or_create_by(name: row["supplier_name"])
-
           item = inventory_pool.items.create(inventory_code: row["inventory_code"].strip,
                                              model: Model.find(row["model_id"]),
-                                             serial_number: (row["serial_number"].blank? ? nil : row["serial_number"]),
-                                             location: location,
-                                             note: (row["note"].blank? ? nil : row["note"]),
                                              is_borrowable: (row["is_borrowable"] == "1" ? 1 : 0),
-                                             is_inventory_relevant: (row["is_inventory_relevant"] == "0" ? 0 : 1),
-                                             invoice_number: (row["invoice_number"].blank? ? nil : row["invoice_number"]),
-                                             invoice_date: (row["invoice_date"].blank? ? nil : row["invoice_date"]),
-                                             price: (row["price"].blank? ? nil : row["price"]),
-                                             supplier: supplier )
+                                             is_inventory_relevant: (row["is_inventory_relevant"] == "0" ? 0 : 1)) do |i|
+            i.serial_number = row["serial_number"] unless row["serial_number"].blank?
+            i.note = row["note"] unless row["note"].blank?
+            i.invoice_number = row["invoice_number"] unless row["invoice_number"].blank?
+            i.invoice_date = row["invoice_date"] unless row["invoice_date"].blank?
+            i.price = row["price"] unless row["price"].blank?
+            i.supplier = Supplier.find_or_create_by(name: row["supplier_name"]) unless row["supplier_name"].blank?
+            unless row["building"].blank? and row["room"].blank?
+              building_id = row["building"].blank? ? nil : Building.find_or_create_by(name: row["building"]).id
+              room = row["room"].blank? ? nil : row["room"]
+              i.location = Location.find_or_create(building_id: building_id, room: room)
+            end
+            i.properties[:anschaffungskategorie] = row["properties_anschaffungskategorie"] unless row["properties_anschaffungskategorie"].blank?
+          end
+
           item.valid?
           items << item
         end
